6713ad1cc58e743c8d3e2decebc341ba3272fa0c
unknown <evanshango>
2020-04-19 20:08:26 +0300

    initial commit

---
+++ app/src/main/java/com/bruce/raeasy/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.bruce.raeasy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bruce.raeasy.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/bruce/raeasy/activities/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.bruce.raeasy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.bruce.raeasy.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
+
+        btnLogin.setOnClickListener(v -> startActivity(new Intent(this, LoginActivity.class)));
+        btnRegister.setOnClickListener(v -> startActivity(new Intent(this, RegisterActivity.class)));
+    }
+}
